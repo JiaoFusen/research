0f2a564ed33623cb419d260f1fc713f27a2087b5
JiaoFusen <jiaorocks@gmail.com>
2018-02-26 17:22:44 +0800

    添加了mean.py模块，整合itemrank模块

--- ItemRank.py
+++ ItemRank.py
@@ -1,9 +1,10 @@
 #!/user/bin/python
 # -*- coding: utf-8 -*-
 import numpy as np
+import pandas as pd
 from itertools import islice
 from sklearn.model_selection import train_test_split
-np.set_printoptions(threshold=np.inf)
+# np.set_printoptions(threshold=np.inf)
 
 class Node(object):
     def __init__(self):
@@ -23,8 +24,11 @@ class ItemRank(object):
         # node = Node()
         # print("******生成图模型中......")
         self.movie_names = list(set(self.data[:, 1].astype(int)))
+        # self.movie_names = set(self.data.movieId.tolist())
+        print(self.movie_names)
         self.user_names = list(set(self.data[:, 0].astype(int)))
-        # print(self.movie_names.index(65567))
+        # print(self.user_names)
+        # self.user_names = set(self.data.userId.tolist())
         self.movie_nodes = {}
         self.user_nodes = {}
         for movie in self.movie_names:
@@ -69,20 +73,25 @@ class ItemRank(object):
 
 
 if __name__ == "__main__":
-    with open("ratings.csv") as file:
-        data = []
-        for line in islice(file, 1, None):
-            data.extend(line.rstrip("\n").split(","))
-    np_data = np.array(data).reshape(-1, 4)
+    # with open("/Users/JiaoFusen/Desktop/ml-latest-small/ratings.csv") as file:
+    #     data = []
+    #     for line in islice(file, 1, None):
+    #         data.extend(line.rstrip("\n").split(","))
+    # np_data = np.array(data).reshape(-1, 4)
+    pd_data = pd.read_csv("/Users/JiaoFusen/Desktop/ml-latest-small/ratings.csv")
+    # print(pd_data)
+    np_data = pd_data.values
+    print(np_data)
     # train_data, test_data = train_test_split(np_data, train_size=0.8)
     train_data = np_data
     item_rank = ItemRank(train_data)
     item_rank.generate_graph()
     # item_rank.generate_coef_from_graph()
     # 选取405号用户来进行计算
-    d = item_rank.generate_d(user_name=665)
-    IR = np.ones(len(item_rank.movie_names))
-    IR = d
+    # d = item_rank.generate_d(user_name=665)
+    # IR = np.ones(len(item_rank.movie_names))
+    # IR = d
+    # 迭代计算
     # covered = False
     # counter = 0
     # while not covered:
--- mean.py
+++ mean.py
@@ -1,3 +1,33 @@
 #!/user/bin/python
 # -*- coding: utf-8 -*-
 # 以平均分为评价指标来测度用户兴趣的演化
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+class Mean(object):
+    def __init__(self, pd_data):
+        self.rating = pd_data
+        self.user_movie = []
+
+    #   计算平均分
+    def calc_mean(self, user):
+        movie_rating = self.rating.loc[:, ['movieId', 'rating']]
+        average_rating = movie_rating.groupby('movieId').mean()
+        user_rating = self.rating[self.rating.userId == user].sort_index(by='timestamp')
+        self.user_movie = user_rating.movieId.tolist()  # 用户看过的电影集合
+        user_rating = user_rating.loc[:, ['movieId', 'rating', 'timestamp']]
+        user_aver_rating = average_rating.loc[self.user_movie]
+        user_rating = user_rating.merge(user_aver_rating, right_index=True, how="outer", left_on='movieId')
+        user_rating1 = user_rating.loc[:, ['rating_x', 'rating_y']]
+        # user_rating1.reindex(np.arange(len(user_rating1)))
+        index = np.arange(len(user_rating1))
+        user_rating1.set_index(index)
+        print(user_rating1)
+
+
+if __name__ == "__main__":
+    pd_data = pd.read_csv("/Users/JiaoFusen/Desktop/ml-latest-small/ratings.csv")
+    mean = Mean(pd_data)
+    mean.calc_mean(288)
